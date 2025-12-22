71ee487f915e5c138f7686b8312731a51bf831f3
Nidhal Ghdiri <ghdiri.nidhal@gmail.com>
2025-12-22 16:32:48 +0400

    Add Units & Tasks 2

--- app/api/tasks/[id]/photos/route.js
+++ app/api/tasks/[id]/photos/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
-import { prisma } from "@/lib/prisma";
+import prisma from "@/lib/prisma";
 
 // GET all photos for a task
 export async function GET(request, { params }) {
--- app/api/units/[id]/inspections/route.js
+++ app/api/units/[id]/inspections/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
-import { prisma } from "@/lib/prisma";
+import prisma from "@/lib/prisma";
 
 // GET all inspections for a specific unit
 export async function GET(request, { params }) {
